6c69bb8c97402997a1b51d9b1e55bca4b9d074d5
Maeruth22 <72513305+Maeruth22@users.noreply.github.com>
2021-06-01 10:42:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    center:true,
+    nav:false,
+    dotsEach:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
